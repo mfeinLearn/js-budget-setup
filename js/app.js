b6cf7e63eca0a62d0160c6550791003457821a04
github username <github email address>
2020-10-15 17:42:25 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -15,4 +15,45 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  //submit budget method
+  submitBudgetForm() {
+      const value = this.budgetInput.value
+      if (value === '' || value < 0) {
+          this.budgetFeedback.classList.add("showItem")
+          this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+          const self = this;
+          console.log("setting this to self: ", this)
+          setTimeout(function() {
+              console.log("console logging this: ", this)
+              console.log("console logging self: " ,self)
+this.budgetFeedback.classList.remove("showItem")
+          }, 4000)
+      }
+  }
 }
+
+function eventListeners() {
+    const budgetForm = document.getElementById('budget-form')
+    const expenseForm = document.getElementById('expense-form')
+    const expenseList = document.getElementById('expense-list')
+
+    // new instance of UI CLASS
+    const ui = new UI()
+
+    // budget from submit
+    budgetForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        ui.submitBudgetForm();
+    })
+    // expense from submit
+    expenseForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+    })
+    // expense click
+    expenseList.addEventListener('click', function() {})
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+})
